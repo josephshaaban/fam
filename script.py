8cbd8862a608a8a4fffe03f41a2851d90faba888
Youssef Shaaban <joseph.sha3ban@gmail.com>
2023-10-24 03:13:18 +0400

    Cleaning up and documenting

--- script.py
+++ script.py
@@ -10,67 +10,59 @@ from utils import (
 )
 
 
+# Reading the data json file
 fn = r'data.json'
-
 with open(fn, encoding="UTF-8") as f:
   data = json.load(f)
 
+# Use our helper class to cast loaded json into custom dict
 data = CustomDict(data)
 
 # Collects some info about Data
 print("Depth of data obj:", data.depth)
 print("Depth of properties obj:", dict_depth(data['properties']))
 
+# Extracts the fields that need to be normalized and casted
 obj_fields, arr_fields = dict_fields_with_levels(data['properties'])
 print(f"Objects fields: {obj_fields}")
 print(f"Array fields: {arr_fields}")
+
+# Be sure our data has no deeper fields
 sub_fields_depth(data, obj_fields, prefix='object')
 sub_fields_depth(data, arr_fields, prefix='array')
 
-# The length of all data is: 25
-# data['properties'][index]["live_viewing"] all the values are None
-# price are: nested objects
+# read data as pandas data frame
 df = pd.DataFrame(data['properties'])
-v = df.drop(arr_fields, axis=1)
+properties = df.drop(arr_fields, axis=1)
 
-# some of your records seem NOT to have `Tags` key, hence `KeyError: 'Tags'`
-# let's fix it
-# price_keys = set()
-# price_key_types = list()
-# for r in data['properties']:
-#   price_keys.update(r['price'].keys())
-
-# for val in data['properties'][1]['price'].values():
-#   price_key_types.append(val)
-
-# print(price_keys, price_key_types)
-# for r in data['properties']:
-#     for price_key in price_keys:
-#         if price_key in r['price']:
-#             print(r['price'][price_key])
-
-price = pd.json_normalize(data['properties'], record_path='images')
 # Normalize dropped fields to be converted to a new table
 normalized_fields = {}
 for field in arr_fields:
   normalized_fields[field] = pd.json_normalize(
     data['properties'],field, ['id'], meta_prefix='parent_')
 
-# con = sqlite3.connect("db.sqlite")
+# Postgres configs
 PG_USER = "postgres"
 PG_PASS = "admin"
 PG_HOST = "localhost"
 PG_PORT = "5432"
 PG_DB_NAME = "fam"
 PG_CLIENT_ENCODING = "utf8"
+
+# create Postgres connection via Alchemy
 eng = create_engine(
     f"postgresql+psycopg2://{PG_USER}:{PG_PASS}@{PG_HOST}:{PG_PORT}/{PG_DB_NAME}",
-    use_native_hstore=False, #isolation_level="REPEATABLE READ"
+    use_native_hstore=False,
+    # isolation_level="Autocommit",
 )
-v.to_sql(
+
+# Create the main table `properties`
+properties.to_sql(
   'properties', con=eng, if_exists='replace', index=True,
+  # cast dict fields to Json fields in the table
   dtype={field: sqlalchemy_types.JSON for field in obj_fields})
 
+# Create new tables in database for each normalized field
 for normalized_field_name, normalized_field_val in normalized_fields.items():
   normalized_field_val.to_sql(
     normalized_field_name, con=eng, if_exists='replace', index=True)
@@ -78,3 +70,4 @@ for normalized_field_name, normalized_field_val in normalized_fields.items():
 print()
 print("===========================================================")
 print("All records has inserted in the corresponding tables. DONE!")
+print()
--- utils.py
+++ utils.py
@@ -1,4 +1,17 @@
-def dict_depth(d):
+"""
+A module for some helper functions and classes.
+"""
+
+
+def dict_depth(d) -> int:
+    """
+    A helper function to discover the depth of multi-level dictionary.
+
+    If the field has value of type list of dicts, this function
+    will calculate the depth of list's dicts with the upper levels.
+
+    Returns the depth of d
+    """
     if isinstance(d, list) and len(d):
         return 0 + max(dict_depth(item) for item in d)
     if isinstance(d, dict) and len(d):
@@ -6,6 +19,14 @@ def dict_depth(d):
     return 0
 
 def dict_fields_with_levels(d, object_fields=set(), array_fields=set()):
+    """
+    A helper function to explores all nested fields with respect to its type
+    (dict field or list field).
+    This helps to deal with fields that need to normalize and the ones that
+    need to cast its dtype for SQLAlchemy.
+
+    Returns a tuple of two sets represented as object_fields, array_fields
+    """
     for r in d:
         if not isinstance(r,(dict, list)):
             continue
@@ -31,8 +52,14 @@ def sub_fields_depth(data_dict, fields, prefix='data'):
   if len(deep_fields) == 0:
     print(f'All {prefix} fields with 0 depth.')
 
+
+# see https://stackoverflow.com/a/19829714
 class AutoVivification(dict):
-    """Implementation of perl's autovivification feature."""
+    """
+    A class that implements nested dictionaries
+
+    Implementation of perl's autovivification feature.
+    """
     def __getitem__(self, item):
         try:
             return dict.__getitem__(self, item)
@@ -42,6 +69,7 @@ class AutoVivification(dict):
 
 
 class CustomDict(AutoVivification):
+    """A helper class adds depth property"""
     @property
     def depth(self):
         return dict_depth(self)
